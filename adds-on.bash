bcc5e267d2853230dbeb57bdb2d1411f0b6d6df9
DatLaid <datlaid@gmail.com>
2024-10-20 22:45:50 +0700

    done

---
+++ adds-on.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+
+
+kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
+
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.3/deploy/static/provider/cloud/deploy.yaml
+
+
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+
+
+
+n
---
+++ alias.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+echo "Adding kubectl aliases to ~/.bashrc..."
+
+cat <<EOL >> ~/.bashrc
+
+# Kubectl Aliases
+alias k=kubectl
+
+alias kg="k get"
+alias kgp="kg po"
+alias kgd="kg deploy"
+alias kgn="kg nodes"
+alias kgsvc="kg svc"
+alias kging="kg ingress"
+alias kgpv="kg pv"
+alias kgpvc="kg pvc"
+alias kgns="kg ns"
+alias kgsec="kg secret"
+alias kgrs="kg rs"
+alias kgsts="kg sts"
+alias kgiss="kg issuer"
+alias kgc="kg challenge"
+
+# Apply and Delete aliases
+alias ka="k apply"
+alias kdel="k delete"
+alias kdelp="kdel po"
+alias kdeld="kdel deploy"
+alias kdelsvc="kdel svc"
+alias kdeling="kdel ingress"
+alias kdelpv="kdel pv"
+alias kdelpvc="kdel pvc"
+alias kdelns="kdel ns"
+alias kdelsec="kdel secret"
+alias kdelrs="kdel rs"
+alias kdelsts="kdel sts"
+
+# Describe aliases
+alias kd="k describe"
+alias kdp="kd po"
+alias kdd="kd deploy"
+alias kdsvc="kd svc"
+alias kding="kd ingress"
+alias kdpv="kd pv"
+alias kdpvc="kd pvc"
+alias kdns="kd ns"
+alias kdsec="kd secret"
+alias kdrs="kd rs"
+alias kdsts="kd sts"
+alias kdc="kd challenge"
+
+# Config aliases
+alias kgctx="k config get-contexts"
+alias kcurctx="k config current-context"
+alias kusectx="k config use-context"
+alias kusens="k config set-context --current"
+
+# Top aliases
+alias kt="k top"
+alias ktp="kt po"
+alias ktn="kt nodes"
+
+EOL
+
+
+source ~/.bashrc
