c89707b45e4961c9038f8103a2e3896d9e99d124
datlaid <datlaid@gmail.com>
2024-11-19 17:27:01 +0700

    add script to join master

--- HA-cluster/add-more-master-node.sh
+++ HA-cluster/add-more-master-node.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-IP_ADDRESS=$(ip -4 addr show ens33 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+#VIP of the load balancer
+LB_VIP="10.0.0.1"
 
+# IP of the node you want to join in the cluster
+ORTHER_MASTER_NODE_IP="10.0.0.2"  
+
+TOKEN=$(kubeadm token create)
 
 CERT_UPLOAD=$(kubeadm init phase upload-certs --upload-certs)
 
+CA_HASH=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | \
+          openssl rsa -pubin -outform DER 2>/dev/null | \
+          openssl dgst -sha256 -hex | awk '{print $2}')
 
-
-
-
-
-kubeadm join ${MASTER_NODE_IP}:6443 --token abcdef.0123456789abcdef \
-    --discovery-token-ca-cert-hash sha256:8f4b344c8d6a33c7b45e1b7a3d23c4e4fb6322a7bff9c4d1b7e68fd70d96f89a \
-    --control-plane --certificate-key 8f374ae5453b7ae304bb9c4c4bfb2d8a4c634b6a933bfa5a5dfd4c2d6e4e8d12 \
-    --apiserver-advertise-address=${ORTHER_MASTER_NODE_IP}
+kubeadm join $LB_VIP:6443 --token $TOKEN \
+    --discovery-token-ca-cert-hash sha256:$CA_HASH \
+    --control-plane --certificate-key $CERT_UPLOAD \
+    --apiserver-advertise-address=$ORTHER_MASTER_NODE_IP
