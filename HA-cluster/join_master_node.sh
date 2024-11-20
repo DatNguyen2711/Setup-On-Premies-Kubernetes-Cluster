878f82f221b21e602f0d4400779c28ee108d100f
datlaid <datlaid@gmail.com>
2024-11-20 14:50:23 +0700

    add join master cmd

--- HA-cluster/join_master_node.sh
+++ HA-cluster/join_master_node.sh
@@ -4,11 +4,11 @@
 LB_VIP="10.0.0.1"
 
 # IP of the node you want to join in the cluster
-ORTHER_MASTER_NODE_IP="10.0.0.2"  
+ORTHER_MASTER_NODE_IP="192.168.10.111"  
 
 TOKEN=$(kubeadm token create)
 
-CERT_UPLOAD=$(kubeadm init phase upload-certs --upload-certs)
+CERT_UPLOAD=$(kubeadm init phase upload-certs --upload-certs | grep -oP '(?<=Using certificate key: )[a-f0-9]+')
 
 CA_HASH=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | \
           openssl rsa -pubin -outform DER 2>/dev/null | \
@@ -16,5 +16,6 @@ CA_HASH=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | \
 
 kubeadm join $LB_VIP:6443 --token $TOKEN \
     --discovery-token-ca-cert-hash sha256:$CA_HASH \
-    --control-plane --certificate-key $CERT_UPLOAD \
-    --apiserver-advertise-address=$ORTHER_MASTER_NODE_IP
+    --certificate-key $CERT_UPLOAD \
+    --apiserver-advertise-address=$ORTHER_MASTER_NODE_IP \
+    --control-plane 
